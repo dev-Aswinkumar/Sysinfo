15100338f8b43b51cbde5a5a753f6aea9d518ca6
Aswin kumar <aswinkumar122008@gmail.com>
2026-09-01 20:40:33 +0530

    simplified the structure of the program

--- main.c
+++ main.c
@@ -1,45 +1,36 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/utsname.h>
-struct utsname getme;
+char* failed="cannot detect";
 char*  get_hostname(){
   static char hostname[256];
   if(gethostname(hostname,sizeof(hostname))==0){
     return hostname;
   }
   else{
-    return "cannot detect";
-  }
-}
-
-int sysd_info(){
-  if (uname(&getme)==0){
-    return 0;
-  }
-  else{
-    return -1;
+    return failed;
   }
 }
 
 
 int main(){
-  printf("System Information\n");
-  printf("------------------\n");
+  struct utsname getme;
   char* hostname = get_hostname();
   char* kernel;
   char* os;
   char* architecture;
-  if(sysd_info()==-1){
-    char* failed="cannot detect";
+  if(uname(&getme)==0){
+    kernel=getme.release;
+    os=getme.sysname;
+    architecture=getme.machine;
+  }
+  else{
     kernel=failed;
     os=failed;
     architecture=failed;
   }
-  else{
-   kernel=getme.release;
-   os=getme.sysname;
-   architecture=getme.machine;
-  }
+  printf("System Information\n");
+  printf("------------------\n");
   printf("Hostname: %s\n",hostname);
   printf("Os: %s\n",os);
   printf("Kernel: %s\n",kernel);
